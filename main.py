3212e8dafdff7227eb1bb9c8d619525901234f4a
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-19 12:01:04 +0200

    Redo PIN validation functions

--- main.py
+++ main.py
@@ -57,6 +57,19 @@ def showInvalidMessage(message):
         ''')
 
 
+def show_card_blocked():
+    os.system('clear')
+    print('''
+========================================
+    THE PIN FOR THIS CARD HAS BEEN
+    ENTERED THREE TIMES INCORRECTLY.
+    THE CARD IS NOW BLOCKED.
+    PLEASE CONTACT YOUR BANKING AGENT.
+
+========================================
+    ''')
+
+
 def showMainMenu():
     os.system('clear')
     print('''
@@ -97,25 +110,37 @@ def cardCodeIsValid(cardNumber, code):
     return accounts[cardNumber]['card']['code'] == code
 
 
+def codeIsValid(cardNumber):
+    cardPin = insertCardPin()
+    if cardCodeIsValid(cardNumber, cardPin) is True:
+        return True
+    return False
+
+
+def validate_pin(cardNumber):
+    pin_attempts = 0
+    while pin_attempts < 3:
+        showIsertPinScreen()
+        evaluation = codeIsValid(cardNumber)
+        if evaluation is True:
+            return True
+        elif evaluation is False and pin_attempts < 2:
+            showInvalidMessage('PIN')
+            print('Icorrect PIN. Returning to Insert PIN screen in 5 seconds.')
+            pin_attempts += 1
+            time.sleep(5)
+        elif evaluation is False and pin_attempts == 2:
+            # block_card function that would send the info to the DB
+            show_card_blocked()
+            print('Returning to main screen in 10 seconds.')
+            time.sleep(10)
+            main()
+    return False
+
+
 def stopATMWithMessage(message):
     print(message)
     sys.exit()
-
-
-def codeIsValid(cardNumber):
-
-    currentPINTries = 1
-    maxPINTries = 3
-    while currentPINTries <= maxPINTries:
-
-        cardPin = insertCardPin()
-
-        if True == cardCodeIsValid(cardNumber, cardPin):
-            return True
-
-        currentPINTries = currentPINTries + 1
-
-    return False
 
 
 def getCardDetails(cardNumber):
@@ -141,46 +166,41 @@ def main():
         print('Card Does Not Exist. Returning to main screen in 5 seconds.')
         time.sleep(5)
         main()
-    valid_pin = False
-    while valid_pin is False:
-        showIsertPinScreen()
-        if codeIsValid(cardNumber) is False:
-            showInvalidMessage('PIN')
-            print('Icorrect PIN. Returning to Insert PIN screen in 5 seconds.')
-            time.sleep(5)
-        else:
-            valid_pin = True
-    menu_running = True
-    while menu_running is True:
-        option = showMainMenu()
-        if option == '1':
-            show_card_details_running = True
-            while show_card_details_running is True:
-                os.system('clear')
-                cardDetails = getCardDetails(cardNumber)
-                showCardDetails(cardDetails)
-                deatils_show_selection = input('Select 0 to go back to the main menu.')
-                if deatils_show_selection == '0':
-                    show_card_details_running = False
-                else:
-                    continue
-        if option == '2':
-            show_balance_running = True
-            while show_balance_running is True:
-                os.system('clear')
-                showBalance(cardNumber)
-                balance_show_selection = input('Select 0 to go back to the main menu.')
-                if balance_show_selection == '0':
-                    show_balance_running = False
-                else:
-                    continue
-        if option == '3':
-            print('Under construction.')
-        if option == '0':
-            valid_card = False
-            valid_pin = False
-            menu_running = False
-            main()
+
+    if validate_pin(cardNumber) is False:
+        main()
+    else:
+        menu_running = True
+        while menu_running is True:
+            option = showMainMenu()
+            if option == '1':
+                show_card_details_running = True
+                while show_card_details_running is True:
+                    os.system('clear')
+                    cardDetails = getCardDetails(cardNumber)
+                    showCardDetails(cardDetails)
+                    deatils_show_selection = input('Select 0 to go back to the main menu.')
+                    if deatils_show_selection == '0':
+                        show_card_details_running = False
+                    else:
+                        continue
+            if option == '2':
+                show_balance_running = True
+                while show_balance_running is True:
+                    os.system('clear')
+                    showBalance(cardNumber)
+                    balance_show_selection = input('Select 0 to go back to the main menu.')
+                    if balance_show_selection == '0':
+                        show_balance_running = False
+                    else:
+                        continue
+            if option == '3':
+                print('Under construction.')
+            if option == '0':
+                valid_card = False
+                valid_pin = False
+                menu_running = False
+                main()
 
 if __name__ == "__main__":
     main()
