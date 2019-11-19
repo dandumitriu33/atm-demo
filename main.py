5e0ee682c8957da47463cbc6ea6f1bab190aede2
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-19 12:36:52 +0200

    Add the show card details option

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def showMainMenu():
     print('''
 ========================================
         SELECT AN OPTION BELOW
-1. SHOW ACCOUNT DETAILS
+1. SHOW CARD DETAILS
 2. SHOW BALANCE
 3. WITHDRAW
 0. EXIT
@@ -83,6 +83,24 @@ def showMainMenu():
     ''')
     user_option = input('Select an option: ')
     return user_option
+
+
+def showCardDetails(cardNumber):
+    os.system('clear')
+    card_holder = accounts[cardNumber]['card']['holderName']
+    card_type = accounts[cardNumber]['card']['type']
+    bank_name = accounts[cardNumber]['bank']['name']
+    bank_account_number = accounts[cardNumber]['bank']['accountNumber']
+    print('''
+========================================
+             CARD DETAILS'''
+    f'\nHolder Name     : {card_holder}'
+    f'\nCard Type       : {card_type}'
+    f'\nBank Name       : {bank_name}'
+    f'\nCurrent Account : {bank_account_number}'
+    '''
+========================================"
+    ''')
 
 
 def showBalance(cardno):
@@ -103,6 +121,11 @@ def showBalance(cardno):
 
 
 def cardExists(cardNumber):
+    # insert function/statement to verify if card is not 'blocked' in DB
+    # if card is 'blocked'
+    #       show_card_blocked()
+    #       main()
+    # else:
     return cardNumber in accounts
 
 
@@ -138,26 +161,43 @@ def validate_pin(cardNumber):
     return False
 
 
-def stopATMWithMessage(message):
-    print(message)
-    sys.exit()
-
-
-def getCardDetails(cardNumber):
-    return accounts[cardNumber]
-
-
-def showCardDetails(cardDetails):
-    print("========================================")
-    print("             SILVER BANK                ")
-    print("Holder Name     : ", cardDetails['card']['holderName'])
-    print("Card Type       : ", cardDetails['card']['type'])
-    print("Bank Name       : ", cardDetails['bank']['name'])
-    print("Current Account : ", cardDetails['bank']['accountNumber'])
-    print("========================================")
+def run_menu(cardNumber):
+    menu_running = True
+    while menu_running is True:
+        option = showMainMenu()
+        if option == '1':
+            show_card_details_running = True
+            while show_card_details_running is True:
+                os.system('clear')
+                showCardDetails(cardNumber)
+                deatils_show_selection = input('Select 0 to go back to the main menu.')
+                if deatils_show_selection == '0':
+                    show_card_details_running = False
+                else:
+                    continue
+        if option == '2':
+            show_balance_running = True
+            while show_balance_running is True:
+                os.system('clear')
+                showBalance(cardNumber)
+                balance_show_selection = input('Select 0 to go back to the main menu.')
+                if balance_show_selection == '0':
+                    show_balance_running = False
+                else:
+                    continue
+        if option == '3':
+            print('Under construction.')
+        if option == '0':
+            valid_card = False
+            valid_pin = False
+            menu_running = False
+            main()
 
 
 def main():
+
+    # first step: validating the card exists and is not blocked
+
     os.system('clear')
     showWelcomeScreen()
     cardNumber = insertCard()
@@ -167,40 +207,16 @@ def main():
         time.sleep(5)
         main()
 
+    # second step: validating the PIN and blocking if it fails 3 times
+
     if validate_pin(cardNumber) is False:
         main()
     else:
-        menu_running = True
-        while menu_running is True:
-            option = showMainMenu()
-            if option == '1':
-                show_card_details_running = True
-                while show_card_details_running is True:
-                    os.system('clear')
-                    cardDetails = getCardDetails(cardNumber)
-                    showCardDetails(cardDetails)
-                    deatils_show_selection = input('Select 0 to go back to the main menu.')
-                    if deatils_show_selection == '0':
-                        show_card_details_running = False
-                    else:
-                        continue
-            if option == '2':
-                show_balance_running = True
-                while show_balance_running is True:
-                    os.system('clear')
-                    showBalance(cardNumber)
-                    balance_show_selection = input('Select 0 to go back to the main menu.')
-                    if balance_show_selection == '0':
-                        show_balance_running = False
-                    else:
-                        continue
-            if option == '3':
-                print('Under construction.')
-            if option == '0':
-                valid_card = False
-                valid_pin = False
-                menu_running = False
-                main()
+
+        # after the security information is verified, shows menu
+
+        run_menu(cardNumber)
+
 
 if __name__ == "__main__":
     main()
