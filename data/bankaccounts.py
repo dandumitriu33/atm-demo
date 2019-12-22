c93700860a09939ed87bbe5109f0e85c0c8a1d06
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-22 20:04:14 +0200

    Add the database and 3 manual test entries

--- data/bankaccounts.py
+++ data/bankaccounts.py
@@ -1,51 +1,51 @@
 accounts = {
     'VS1893': {
-        'card': {
-            'holderName': 'F. Mascarpone',
-            'type': 'VISA',
-            'code': '9832'
-        },
-        'bank': {
-            'name': 'Gold Stealers Inc.',
-            'accountName': 'Francesco Mascarpone',
-            'accountNumber': 'IBAN4923900100019432',
-            'balance': 9383,
-            'currency': 'USD'
-        }
-    },
+                'card': {
+                            'holderName': 'F. Mascarpone',
+                            'type': 'VISA',
+                            'code': '9832'
+                        },
+                'bank': {
+                            'name': 'Gold Stealers Inc.',
+                            'accountName': 'Francesco Mascarpone',
+                            'accountNumber': 'IBAN4923900100019432',
+                            'balance': 9383,
+                            'currency': 'USD'
+                        }
+                },
     'MC8931': {
-        'card': {
-            'holderName': 'G. Terrini',
-            'type': 'MasterCard',
-            'code': '1392'
-        },
-        'bank': {
-            'name': 'Gold Stealers Inc.',
-            'accountName': 'Giovani Terrini',
-            'accountNumber': 'IBAN31008901000018901',
-            'balance': 10432,
-            'currency': 'EUR'
-        }
-    },
+                'card': {
+                            'holderName': 'G. Terrini',
+                            'type': 'MasterCard',
+                            'code': '1392'
+                        },
+                'bank': {
+                            'name': 'Gold Stealers Inc.',
+                            'accountName': 'Giovani Terrini',
+                            'accountNumber': 'IBAN31008901000018901',
+                            'balance': 10432,
+                            'currency': 'EUR'
+                        }
+                },
     'VS19842': {
-        'card': {
-            'holderName': 'A. Fabrizi',
-            'type': 'MasterCard',
-            'code': '1112'
-        },
-        'bank': {
-            'name': 'Gold Stealers Inc.',
-            'accountName': 'Antonio Fabrizi',
-            'accountNumber': 'IBAN1930183277892001',
-            'balance': 103742,
-            'currency': 'EUR'
-        }
-    },
+                'card': {
+                            'holderName': 'A. Fabrizi',
+                            'type': 'MasterCard',
+                            'code': '1112'
+                        },
+                'bank': {
+                            'name': 'Gold Stealers Inc.',
+                            'accountName': 'Antonio Fabrizi',
+                            'accountNumber': 'IBAN1930183277892001',
+                            'balance': 103742,
+                            'currency': 'EUR'
+                        }
+                },
 }
 
 atmRules = {
-    'commission':{
-        'VISA': 1,
-        'MasterCard': 2
-    }
-}
+            'commission': {
+                            'VISA': 1,
+                            'MasterCard': 2
+                            }
+            }
--- server.py
+++ server.py
@@ -1,11 +1,64 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atm.db'
+db = SQLAlchemy(app)
+
+
+class Atm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, nullable=False)
+    card_type = db.Column(db.String, default='')
+    card_pin = db.Column(db.Integer, nullable=False)
+    holder_name = db.Column(db.String, default='')
+    bank_name = db.Column(db.String, default='')
+    account_name = db.Column(db.String, default='')
+    account_number = db.Column(db.String, default='')
+    balance = db.Column(db.Float, default=0)
+    currency = db.Column(db.String, default='')
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        entry_card = request.form['card']
+        entry_card_type = request.form['card-type']
+        entry_card_pin = request.form['card-pin']
+        entry_holder_name = request.form['holder-name']
+        entry_bank_name = request.form['bank-name']
+        entry_account_name = request.form['account-name']
+        entry_account_number = request.form['account-number']
+        entry_balance = request.form['balance']
+        entry_currency = request.form['currency']
+        new_entry = Atm(card_id=entry_card,
+                        card_type=entry_card_type,
+                        card_pin=entry_card_pin,
+                        holder_name=entry_holder_name,
+                        bank_name=entry_bank_name,
+                        account_name=entry_account_name,
+                        account_number=entry_account_number,
+                        balance=entry_balance,
+                        currency=entry_currency)
+        try:
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect(url_for('admin'))
+        except:
+            return 'There was an issue adding the card.'
+
+    else:
+        # cards = Atm.query.order_by(Atm.card_id).all() # also add cards=cards in return and jinja
+        return render_template('admin.html')
 
 
 if __name__ == "__main__":
